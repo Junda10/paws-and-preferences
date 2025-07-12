3b88e03573503ea6d9837bef3b7e8597399b9b5a
Chia Chin Tat <chiachintat10@gmail.com>
2025-07-13 03:33:58 +0800

    Fix fetch api

--- paws-and-preferences/src/services/cataas.js
+++ paws-and-preferences/src/services/cataas.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const fetchRandomCat = async () => {
-  const { data } = await axios.get('/cat', {
+  const { data } = await axios.get('https://cataas.com/cat', {
     params: { json: true },
     headers: { Accept: 'application/json' }
   });
-  return data.url.startsWith('http')
+  return data.url && data.url.startsWith('http')
     ? data.url
     : `https://cataas.com${data.url}`;
 };
